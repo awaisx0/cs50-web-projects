8cda93a570bece1148e5d1f2e3c89d9720ee571e
Awais <mawaiskhera.674@gmail.com>
2026-01-07 15:25:47 +0500

    wiki entry page, index, random page work

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+# app_name = "encyclopedia"
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("random", views.random_page, name="random"),
+    path("<str:title>", views.page, name="page"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+from markdown2 import Markdown
+import random
+import pdb
+import ipdb
 
 from . import util
 
@@ -8,3 +14,20 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def page(request, title):
+    entry = util.get_entry(title)
+    markdowner = Markdown()
+    entry_html = markdowner.convert(str(entry))
+    return render(request, "encyclopedia/page.html", {
+        "title": title,
+        "entry_html": entry_html
+    })
+    
+def random_page(request):
+    print("random called")
+    entries_list = util.list_entries()
+    print(entries_list)
+    random_entry = random.choice(entries_list)
+    print(random_entry)
+    return HttpResponseRedirect(reverse("page", args=[random_entry]))
+    # return HttpResponse("random")
