fe7f3d322bd95c23d331165e0a73706e20c1240e
Awais <mawaiskhera.674@gmail.com>
2026-01-14 09:38:08 +0500

    commerce models define

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,26 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Auction(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=500)
+    starting_bid = models.DecimalField(max_digits=8, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    won_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    img_url = models.URLField(blank=True, null=True) # optional
+    category = models.CharField(max_length=25, blank=True, null=True) # optional
+
+    
+class Bid(models.Model):
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    bid_price = models.DecimalField(max_digits=8, decimal_places=2)
+    bid_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+class Comment(models.Model):
+    comment_text = models.CharField(max_length=255)
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    comment_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    
